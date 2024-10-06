99badce62856cb133cc0190bda4141a7d6ba0fa8
AnaCatalina <93290449+AnaCatalina@users.noreply.github.com>
2024-10-06 00:36:26 -0300

    Mejoras en Interfaces

---
+++ Assets/Scripts-Menus/Interface/Creditos.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Creditos : MonoBehaviour
+{
+    // Este método se llama al iniciar la escena
+    // Invoca el método "LoadMainMenu" después de 31 segundos, que cambiará a la escena "MainMenu"
+    void Start()
+    {
+        Invoke("LoadMainMenu", 35);
+    }
+
+    // Este método se llama una vez por frame
+    // Revisa si se presiona Escape, la barra espaciadora, Enter o el clic derecho del mouse, y cambia a la escena "MainMenu"
+    void Update()
+    {
+        // Verifica si alguna de las teclas son presionadas
+        if (Input.GetKeyDown(KeyCode.Escape) ||
+            Input.GetKeyDown(KeyCode.Space) ||
+            Input.GetKeyDown(KeyCode.Return))
+        {
+            LoadMainMenu();
+        }
+    }
+
+    // Este método cambia a la escena "MainMenu"
+    // Se invoca automáticamente tras 31 segundos o cuando es llamado
+    void LoadMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
